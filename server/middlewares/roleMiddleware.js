dceb79d282c7ff938cd00dee9dc4985280c122f7
kiranspradeep <kiranspradeep2002@gmail.com>
2025-02-01 18:33:43 +0530

    sdfsa

--- server/middlewares/roleMiddleware.js
+++ server/middlewares/roleMiddleware.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-const jwt = require('jsonwebtoken');
-const OrganizerAdmin = require('../models/OrganizerAdminModel');
-
-const roleMiddleware = (roles) => async (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
-  if (!token) return res.status(401).json({ message: 'Access denied. No token provided.' });
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRETKEY);
-    const user = await OrganizerAdmin.findById(decoded.userId);
-    if (!user) return res.status(404).json({ message: 'User not found' });
-
-    req.user = { _id: user._id, role: user.role };
-    if (!roles.includes(user.role)) return res.status(403).json({ message: 'Access denied' });
-
-    next();
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: error.message || 'Server error' });
-  }
-=======
 const roleMiddleware = (allowedRoles) => (req, res, next) => {
   const userRole = req.user?.role;
   // console.log(req.user);
@@ -30,8 +8,6 @@ const roleMiddleware = (allowedRoles) => (req, res, next) => {
   }
 
   next();
->>>>>>> main
 };
 
 module.exports = roleMiddleware;
-
